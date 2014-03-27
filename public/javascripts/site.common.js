7284b5cdfe4c906a0cc4d3c02565edd45b2d0184
jamiederycke <jamie.derycke@moneysupermarket.com>
2014-03-27 23:51:07 +0000

    Slick Drive msm quotes - hardcoded

--- public/javascripts/site.common.js
+++ public/javascripts/site.common.js
@@ -1,9 +1,58 @@
+var windowsize = $(window).width();
+
 $(document).ready(function () {
 	$('a.details-link').on('click', function(e) {
 
-		//$('#widgetVehicle').html('');
-		//$('#widgetPrice').html($(this).find('.price').html());
+		$('#widgetVehicle').html($(this).parent().find('.vehicle').html());
+		$('#widgetPrice').html($(this).parent().find('.price').html());
+
+		$('#widgetImage').prop('src', $(this).parent().find('.logo img').prop('src'));
+
+		if(windowsize <= 767) {
+			$('ol.results-table').fadeOut('slow');
+		} else {
+			$('ol.results-table').fadeIn('slow');
+		}
+		$('div.details').fadeIn('slow');
+		$('.quotes').fadeIn('slow');
+
+		var url = 'http://api.msmeco.co.uk/vehicles/insurance/a1';
+		$.ajax({
+			type: 'GET',
+        	url: url,
+        	async: false,
+			contentType: "application/json",
+    		dataType: 'jsonp',
+    		success: function(json) {
+    			alert(json)
+    		},
+    		error: function(e) {
+		       alert(e.message);
+		    }
+
+			// var results = [];
+
+			// $.each(data, function(i){
+			// 	results.push( "<li id='" + i + "'>" + i.coverType + "</li>" );
+			// 	alert(key);
+			// });
+
+
+
+			// $( "<ul/>", {"class": "my-new-list",
+			// 	html: items.join( "" )
+  	// 		}).appendTo( "div.results-container" );            
+		});
 
 		return false;
 	})
+
+	$('a.back-to-results').on('click', function(e) {
+		$('ol.results-table').fadeIn('slow');
+		$('div.details').fadeOut('slow');
+	})
+});
+
+$(window).resize(function() {
+	windowsize = $(window).width();
 });
